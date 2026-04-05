b99f604d92c76d0e1346f20ed5eed51ea805bede
nicole-dev-tech <nmakayi@byupathway.edu>
2026-04-05 16:49:45 +0200

    review

--- scripts/review.js
+++ scripts/review.js
@@ -1,23 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  const products = [
+    { id: "fc-1888", name: "Flux Capacitor" },
+    { id: "fc-2050", name: "Power Laces" },
+    { id: "fs-1987", name: "Time Circuits" },
+    { id: "ac-2000", name: "Low Voltage Reactor" },
+    { id: "jj-1969", name: "Warp Equalizer" }
+  ];
 
+  const select = document.getElementById("product");
 
-        const products = [
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    select.appendChild(option);
+  });
 
-            { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-            { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-            { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-            { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-            { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
-            ];
-
-
-        const select = document.getElementById("product");
-
-        products.forEach(product => {
-        const option = document.createElement("option");
-        option.value = product.id;
-        option.textContent = product.name;
-        select.appendChild(option);
-        });
-    })
+});
